fecbfe4ded80cba5402b5f46d2e72f0bcd078290
Maria Schoinaki <shinaki54@gmail.com>
2025-02-04 12:27:32 +0200

    experiment 1

--- ithaca/util/eval.py
+++ ithaca/util/eval.py
@@ -463,7 +463,7 @@ def compute_attribution_saliency_maps(text_char,
       text_word_onehot,
       text_len=text_len,
       alphabet=alphabet)
-  date_saliency = np.clip(grad_char, 0, 1) #+ grad_word
+  date_saliency = np.clip(grad_char + grad_word , 0, 1) #
 
   return date_saliency, subregion_saliency
 
